d22ea38e24f7e9e2118c00ab40fab84f2e508b07
Tom Payne <t.payne@ticketarena.co.uk>
2015-07-17 13:25:34 +0100

    Removed test view

--- src/views/Routes.jsx
+++ src/views/Routes.jsx
@@ -2,13 +2,11 @@ import { Route, DefaultRoute, NotFoundRoute } from 'react-router';
 import React from 'react';
 
 import Main from './Main';
-import Test from './Test';
 import NotFound from './NotFound';
 
 export default (
 	<Route path="/">
 		<DefaultRoute handler={Main} />
-		<Route path="/test" handler={Test} />
 		<NotFoundRoute name="404" handler={NotFound} />
 	</Route>
 );
