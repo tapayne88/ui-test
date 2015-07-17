abd0d5787d8bdd11c220f2611accd8dd138da99d
Tom Payne <t.payne@ticketarena.co.uk>
2015-07-17 11:33:39 +0100

    Added bunyan logger

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import React from 'react';
 import Router from 'react-router';
-import routes from './views/Routes';
 import util from 'util';
+import bunyan from 'bunyan';
+
+import routes from './views/Routes';
 
 let app = express();
+let log = bunyan.createLogger({name: 'ISOBet'});
 
 function getHtml() {
 	return (`
@@ -23,6 +26,7 @@ app.use(express.static('static'));
 
 // Ask react router to route
 app.use((req, res, next) => {
+	log.info('serving request [%s]', req.url);
 	Router.run(routes, req.url, (Handler, router) => {
 		let reactApp = React.renderToString(<Handler />);
 
@@ -32,6 +36,7 @@ app.use((req, res, next) => {
 
 // Handle them 500's
 app.use((err, req, res, next) => {
+	log.error(err);
 	let message = '<h1>Oh no! Something went gone wrong... sorry</h1>';
 	return res.status(500).send(util.format(getHtml(), message));
 });
@@ -40,5 +45,5 @@ let server = app.listen('3000', () => {
 	let host = server.address().address;
 	let port = server.address().port;
 
-	console.log(`Server running at http://${host}:${port}`);
+	log.info(`Server running at http://${host}:${port}`);
 });
