864b00dfb59affa17198ff586e6213ec1af048e4
Tom Payne <t.payne@ticketarena.co.uk>
2015-07-18 17:03:56 +0100

    Wired up component state fetching

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -4,25 +4,51 @@ import {Link} from 'react-router';
 import Header from './components/header';
 import Table from './components/table';
 
+import BetStore from '../stores/betStore';
+import showBets from '../actions/showBets';
+
 export default class Main extends React.Component {
 
-	constructor(context) {
-		super(context);
+	constructor(props) {
+		super(props);
 
-		this.betStore = context.context.getStore('BetStore');
-		this.bets = this.betStore.getAll();
+		this.getStore = props.context.getStore;
+		this.executeAction = props.context.executeAction;
+
+		this.state = this.getStateFromStore();
+	}
+
+	componentDidMount() {
+		this.getStore(BetStore).addChangeListener(() => { this.handleBetStoreChange() });
+	}
+
+	componentWillUnmount() {
+		this.getStore(BetStore).removeChangeListener(() => { this.handleBetStoreChange() });
+	}
+
+	getStateFromStore() {
+		let betStore = this.getStore(BetStore);
+
+		return {
+			bets: betStore.getAll()
+		}
+	}
+
+	handleBetStoreChange() {
+		this.setState(this.getStateFromStore());
 	}
 
 	render() {
 		return (
 			<div>
 				<Header />
-				<Table data={this.bets} action={this.addToBetslip} />
+				<Table data={this.state.bets} action={(data) => { this.addToBetslip(data) }} />
 			</div>
 		);
 	}
 
 	addToBetslip(data) {
+		this.executeAction(showBets);
 		console.log(`adding to betslip ${data.bet_id}`);
 	}
 }
--- src/views/components/table.jsx
+++ src/views/components/table.jsx
@@ -10,19 +10,18 @@ class TableRow extends React.Component {
 	render() {
 		let data = this.props.data;
 
-		let tds = [];
+		let row = [];
 		Object.keys(this.props.data).forEach((key) => {
-			tds.push(<td key={key}>{data[key]}</td>);
+			row.push(<td key={key}>{data[key]}</td>);
 		});
-		let row = <tr>{tds}</tr>;
 
 		if (this.props.action) {
 			return (
-				<a onClick={this.props.action.bind(null, data)}>{row}</a>
+				<tr onClick={this.props.action.bind(null, data)}>{row}</tr>
 			);
 		}
 
-		return row;
+		return <tr>{row}</tr>;
 	}
 }
 
