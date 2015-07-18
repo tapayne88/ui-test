99331cc47e64549a6a1085df15b64842c33a3fcb
Tom Payne <t.payne@ticketarena.co.uk>
2015-07-18 17:17:21 +0100

    Made action fetch data from api

--- src/actions/showBets.js
+++ src/actions/showBets.js
@@ -1,12 +1,13 @@
-let data = [
-{ "bet_id": 1, "event": "World Cup 2018", "name": "England", "odds": { "numerator": 10, "denominator": 1 } },
-{ "bet_id": 2, "event": "World Cup 2018", "name": "Brazil", "odds": { "numerator": 1, "denominator": 1 } },
-{ "bet_id": 3, "event": "World Cup 2018", "name": "Spain", "odds": { "numerator": 3, "denominator": 1 } },
-{ "bet_id": 4, "event": "Next General Election", "name": "Labour", "odds": { "numerator": 7, "denominator": 4 } },
-{ "bet_id": 5, "event": "Next General Election", "name": "Conservatives", "odds": { "numerator": 2, "denominator": 1 } },
-{ "bet_id": 6, "event": "Next General Election", "name": "Liberal Democrats", "odds": { "numerator": 17, "denominator": 1 } }
-];
+import superagent from 'superagent';
 
-export default function showBets(actionContext, payload) {
-	actionContext.dispatch('RECEIVE_BETS', data);
+export default function showBets(actionContext, payload, done) {
+	superagent
+		.get('http://skybettechtestapi.herokuapp.com/available')
+		.set('Accept', 'application/json')
+		.end((err, res) => {
+			if (err) return done(err);
+
+			actionContext.dispatch('RECEIVE_BETS', res.body);
+			return done();
+		});
 }
